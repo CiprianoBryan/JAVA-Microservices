fe3350df88de026a7c1909360dab4a64dce3c57e
Bryan Cipriano <ciprianot.bryan@gmail.com>
2021-09-25 01:43:23 -0500

    Get Configuration from application properties

--- src/main/java/com/arimagroup/limitsservice/controller/LimitsController.java
+++ src/main/java/com/arimagroup/limitsservice/controller/LimitsController.java
@@ -1,13 +1,18 @@
 package com.arimagroup.limitsservice.controller;
 
 import com.arimagroup.limitsservice.bean.Limits;
+import com.arimagroup.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
