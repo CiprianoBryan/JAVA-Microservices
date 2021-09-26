5b28343c89012286ccd09194409e0beb348d11d9
Bryan Cipriano <ciprianot.bryan@gmail.com>
2021-09-26 01:50:49 -0500

    Create a JPA Repository

--- currency-exchange-service/src/main/java/com/arimagroup/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/arimagroup/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.arimagroup.currencyexchangeservice.controller;
 
 import com.arimagroup.currencyexchangeservice.bean.CurrencyExchange;
+import com.arimagroup.currencyexchangeservice.repository.CurrencyExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +13,16 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
     @Autowired
+    private CurrencyExchangeRepository currencyExchangeRepository;
+    @Autowired
     private Environment environment;
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to find data for " + from + " to " + to);
+        }
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
         return currencyExchange;
