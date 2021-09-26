4055c8e15eaf0ff23a43df704a3e7f0a0342bb44
Bryan Cipriano <ciprianot.bryan@gmail.com>
2021-09-26 06:49:50 -0500

    Setting up Spring Cloud API Gateway

---
+++ api-gateway/src/main/java/com/arimagroup/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.arimagroup.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
