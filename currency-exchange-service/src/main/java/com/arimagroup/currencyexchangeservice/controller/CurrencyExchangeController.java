0ca8e54b5e91ac773736c528b0a1e8bf8071121b
Bryan Cipriano <ciprianot.bryan@gmail.com>
2021-09-29 21:52:23 -0500

    Distributed Tracing - Launching Zipkin Container using Docker - Connecting CurrencyExchange Microservice with Zipkin

--- currency-exchange-service/src/main/java/com/arimagroup/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/arimagroup/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,16 +2,18 @@ package com.arimagroup.currencyexchangeservice.controller;
 
 import com.arimagroup.currencyexchangeservice.bean.CurrencyExchange;
 import com.arimagroup.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
 public class CurrencyExchangeController {
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private CurrencyExchangeRepository currencyExchangeRepository;
     @Autowired
@@ -19,6 +21,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
         if (currencyExchange == null) {
             throw new RuntimeException("Unable to find data for " + from + " to " + to);
